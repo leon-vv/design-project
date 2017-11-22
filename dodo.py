c1c6faf629e04365128d128ae54ba910d946ddae
Léon van Velzen <leon-vv@hotmail.com>
2017-11-22 20:22:03 +0100

    Add doit build script

---
+++ dodo.py
@@ -0,0 +1,28 @@
+files = [
+    ["mux2-to-1", "mux4-to-1"] # Task 1
+]
+
+def prefix_dir(name, suffix, task_num):
+    return "task" + str(task_num) + "/" + name + "/" + name + suffix
+
+def task_all():
+    i = 0
+
+    for directory in files:
+        i += 1
+
+        actions = []
+
+        for name in directory:
+            vhdl_name = name.replace("-", "_")
+            target = "build/task" + str(i) + "/" + name + ".vcd"
+            subactions = [
+                "ghdl_mcode -a " + prefix_dir(name, ".vhd", i),
+                "ghdl_mcode -a " + prefix_dir(name, "-tb.vhd", i),
+                "ghdl_mcode -r " + vhdl_name + "_tb --vcd=" + target + " --stop-time=1000ns "]
+            actions.extend(subactions)
+
+        yield {
+            'basename': "task" + str(i),
+            'actions': actions
+        }
