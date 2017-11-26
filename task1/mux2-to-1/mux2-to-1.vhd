ba246a0f98ef6a6dd0912a94cb5431475c3345a1
Léon van Velzen <leon-vv@hotmail.com>
2017-11-26 21:25:19 +0100

    Rename ports of mux2-to-1.vhd

--- task1/mux2-to-1/mux2-to-1.vhd
+++ task1/mux2-to-1/mux2-to-1.vhd
@@ -3,15 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity mux2_to_1 is
     port(
-        i0 : in std_logic;
-        i1 : in std_logic;
-        s : in std_logic;
-        e : in std_logic;
-        y : out std_logic);
+        D0 : in std_logic;
+        D1 : in std_logic;
+        S : in std_logic;
+        E : in std_logic;
+        O : out std_logic);
 end mux2_to_1;
 
 architecture mux2_to_1_arch of mux2_to_1 is
 begin
-    y <= (i1 and s and e) or (i0 and i1 and s and (not e)) or (i0 and (not s) and e);
+    O <= (D1 and S and E) or (D0 and D1 and S and (not E)) or (D0 and (not S) and E);
 end mux2_to_1_arch;
 
