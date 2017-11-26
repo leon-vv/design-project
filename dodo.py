856a21292a2adba934a80587c6cf85f13092e593
Léon van Velzen <leon-vv@hotmail.com>
2017-11-26 21:26:24 +0100

    Add reg target and increase run time

--- dodo.py
+++ dodo.py
@@ -1,5 +1,5 @@
 files = [
-    ["mux2-to-1", "mux4-to-1"] # Task 1
+    ["mux2-to-1", "mux4-to-1", "reg"] # Task 1
 ]
 
 def prefix_dir(name, suffix, task_num):
@@ -19,7 +19,7 @@ def task_all():
             subactions = [
                 "ghdl_mcode -a " + prefix_dir(name, ".vhd", i),
                 "ghdl_mcode -a " + prefix_dir(name, "-tb.vhd", i),
-                "ghdl_mcode -r " + vhdl_name + "_tb --vcd=" + target + " --stop-time=1000ns "]
+                "ghdl_mcode -r " + vhdl_name + "_tb --vcd=" + target + " --stop-time=3000ns "]
             actions.extend(subactions)
 
         yield {
