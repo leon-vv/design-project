8db4bb49b1b2f4b448a1570e2f4b88dd5a4a6c23
Léon van Velzen <leon-vv@hotmail.com>
2017-12-06 21:32:37 +0100

    Use downto in mux4-2to1.vhd

--- task1/mux4-2to1/mux4-2to1.vhd
+++ task1/mux4-2to1/mux4-2to1.vhd
@@ -3,10 +3,10 @@ use ieee.std_logic_1164.all;
 
 entity mux4_2to1 is
     port(
-        D0 : in std_logic_vector(0 to 3);
-        D1 : in std_logic_vector(0 to 3);
+        D0 : in std_logic_vector(3 downto 0);
+        D1 : in std_logic_vector(3 downto 0);
         S : in std_logic;
-        O : out std_logic_vector(0 to 3));
+        O : out std_logic_vector(3 downto 0));
 end mux4_2to1;
 
 architecture mux4_2to1_arch of mux4_2to1 is
