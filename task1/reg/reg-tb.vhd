5e6bd0c7ff4525d38ac85ec87252c271f680654e
Léon van Velzen <leon-vv@hotmail.com>
2017-12-06 16:28:28 +0100

    Improve register test bench

--- task1/reg/reg-tb.vhd
+++ task1/reg/reg-tb.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity reg_tb is
 end reg_tb;
@@ -19,57 +20,33 @@ architecture reg_tb_arch of reg_tb is
     for test_reg : reg use entity work.reg;
     signal D : std_logic_vector(3 downto 0);
 	signal CE : std_logic;
-	signal C : std_logic;
+	signal C : std_logic := '1';
 	signal R : std_logic;
 	signal Q : std_logic_vector(3 downto 0);
 begin
 
+    C <= not C after 30 ns;
     test_reg: reg port map(D, CE, C, R, Q);
 
     process begin
-		CE <= '1';
-        R <= '1';
-		C <= '0';
-        wait for 100 ns;
-		R <= '0';
-        D <= "1010";
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
-		C <= '0';
-		wait for 100 ns;
-		R <= '1';
-		C <= '1';
-		wait for 100 ns;
-		C <= '0';
-		R <= '0';
-        wait for 100 ns;
-        D <= "0101";
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
-		C <= '0';
-		CE <= '0';
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
-		C <= '0';
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
-		C <= '0';
-		CE <= '1';
-		wait for 100 ns;
-		D <= "1001";
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
-		CE <= '0';
-		C <= '0';
-		wait for 100 ns;
-		C <= '1';
-		wait for 100 ns;
+
+        for I in 0 to 15 loop
+            D <= std_logic_vector(to_unsigned(I, 4));
+
+            for I in std_logic range '0' to '1' loop
+                CE <= I;
+
+                for J in std_logic range '0' to '1' loop
+                    R <= J;
+
+                    wait for 100 ns;
+                end loop;
+
+            end loop;
+
+        end loop;
 
     end process;
 
 end reg_tb_arch;
+
