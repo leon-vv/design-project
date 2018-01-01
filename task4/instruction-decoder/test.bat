db1bb4d9864885ae799982a5fae1c2f1b612da11
Jelle van Cappelle <jelle.jvc@gmail.com>
2018-01-01 16:10:18 +0100

    Removed unnecessary files

--- task4/instruction-decoder/test.bat
+++
@@ -1,4 +0,0 @@
-..\..\ghdl\bin\ghdl.exe -a instruction-decoder.vhd
-..\..\ghdl\bin\ghdl.exe -a instruction-decoder-tb.vhd
-..\..\ghdl\bin\ghdl.exe -r instruction_decoder_tb --vcd=test.vcd --stop-time=6800ns
-pause
